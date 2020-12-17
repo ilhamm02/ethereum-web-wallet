846f039840900c5d93fd5f14f0ee3abd74563e6f
Ilham Maulana <ilhamaulana24@gmail.com>
2020-12-17 07:55:43 +0700

    Create importAccount.php

---
+++ importAccount.php
@@ -0,0 +1,61 @@
+<?php
+ini_set('display_errors', 1); 
+error_reporting(E_ALL);
+
+include 'vendor/autoload.php';
+
+$loader = new \Composer\Autoload\ClassLoader();
+$loader->addPsr4('phpseclib\\', __DIR__ . '/path/to/phpseclib2.0');
+$loader->register();
+		
+use phpseclib\Net\SSH2;
+if(isset($_POST["method"]) && (isset($_POST["par1"]) OR isset($_POST["par2"]))){
+	$par1 = $_POST["par1"];
+	$ssh = new SSH2('62.171.140.49');
+
+	$data = array(
+	            "jsonrpc" => "2.0",
+	            "method" => "personal_importRawKey",
+	            "params"  => array($par1,"<WALLET_PASSWORD>"),
+	            "id" => 83
+	);
+	$ch = curl_init();
+	curl_setopt_array($ch, array(
+	    CURLOPT_URL             => "http://<GETH_IP>:<GETH_PORT>",
+	    CURLOPT_POST            => true,
+	    CURLOPT_POSTFIELDS      => json_encode($data),
+	    CURLOPT_HTTPHEADER      => ["Content-Type: application/json"],
+	    CURLOPT_RETURNTRANSFER  => true,
+	    CURLOPT_HEADER          => false,
+	    CURLOPT_IPRESOLVE       => CURL_IPRESOLVE_V4,
+	));
+	$result = curl_exec($ch);
+	curl_close($ch);
+	$result = json_decode($result);
+	if(isset($result->result)){
+		$address = $result->result;
+		$address1 = explode("0x", $address);
+		$ssh = new SSH2($dokumen["ip_console"]);
+		if (!$ssh->login("<SERVER_IP>", "<SERVER_PASSWORD>")) {
+		    exit('Login Failed');
+		}
+		$ssh->exec('mv <OLD_KEYSTORE_NAME> <NEW_KEYSTORE_NAME>");
+		setCookie("temporaryAddress", $address, strtotime("2050-01-19 03:14:07"));
+		echo "1";
+	}elseif(isset($result->error) && $result->error->message == "account already exists"){
+		$ssh = new SSH2($dokumen["ip_console"]);
+		if (!$ssh->login($dokumen["username_console"], $dokumen["password_console"])) {
+		    exit('Login Failed');
+		}
+		$address = $ssh->exec('cat <KEYSTORE_NAME>');
+		$address = json_decode($address);
+		$address = "0x".$address->address;
+		setCookie("temporaryAddress", $address, strtotime("2050-01-19 03:14:07"));
+		echo "1";
+	}else{
+		echo "[ERROR] ".$result->error->message;
+	}
+}else{
+	echo "[ERROR] Invalid key!!!";
+}
+?>
